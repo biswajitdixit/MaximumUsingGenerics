97613822b594647ae42f2c4f739df0f494122654
biswajitdixit <buntyoec1998@gmail.com>
2021-02-04 21:49:09 +0530

    ADDED: extend the max method

--- src/main/java/com/max/Maximum.java
+++ src/main/java/com/max/Maximum.java
@@ -1,28 +1,35 @@
 package com.max;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
-
+// Generic Class To Find Max Values
 public class Maximum<E extends Comparable<E>> {
     E[] values;
+    List<E> listValues = new LinkedList<>();
+
 
     public Maximum(E[] values) {
         this.values = values;
     }
 
-    public static <E> void printMax(E max) {
-        System.out.println("Max Value = " + max);
+
+    public Maximum(List<E> listValues) {
+        this.listValues = listValues;
     }
 
+    public E findMax() {
+        return Collections.max(listValues);
+    }
 
     public E max() {
         if (values[0].compareTo(values[1]) > 0 && values[0].compareTo(values[2]) > 0) {
-            printMax(values[0]);
             return values[0];
         } else if (values[1].compareTo(values[0]) > 0 && values[1].compareTo(values[2]) > 0) {
-            printMax(values[1]);
             return values[1];
         } else {
-            printMax(values[2]);
             return values[2];
         }
     }
+
 }
--- src/test/java/com/max/MaximumTest.java
+++ src/test/java/com/max/MaximumTest.java
@@ -3,9 +3,12 @@ package com.max;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MaximumTest {
 
-
+    // Test Cases For Integer Array
     @Test
     public void givenFirstIntegerNumberAsLarge_ShouldReturn_FirstNumberAsMax() {
         Integer[] integerValues = {48, 5, 1};
@@ -27,6 +30,7 @@ public class MaximumTest {
         Assert.assertEquals((Integer) 10, max);
     }
 
+    // Test Cases For Float Array
     @Test
     public void givenFirstFloatNumberAsLarge_ShouldReturn_FirstNumberAsMax() {
         Float[] floatValues = {20f, 5f, 1f};
@@ -48,6 +52,7 @@ public class MaximumTest {
         Assert.assertEquals((Float) 100f, max);
     }
 
+    // Test Cases For String Array
     @Test
     public void givenFirstStringAsLarge_ShouldReturn_FirstStringAsMax() {
         String[] stringValues = {"MANGOES", "HELLO", "DAS"};
@@ -56,7 +61,7 @@ public class MaximumTest {
     }
 
     @Test
-    public void givenSecondStringAsLarge_ShouldReturn_SecondStringAsMax() {
+    public void givenSecondStringAsLarge_ShouldReturn_ThirdStringAsMax() {
         String[] stringValues = {"HELLO", "MANGOES", "DAS"};
         String max = new Maximum<>(stringValues).max();
         Assert.assertEquals("MANGOES", max);
@@ -66,6 +71,42 @@ public class MaximumTest {
     public void givenThirdStringAsLarge_ShouldReturn_ThirdStringAsMax() {
         String[] stringValues = {"HELLO", "DAS", "MANGOES"};
         String max = new Maximum<>(stringValues).max();
+        Assert.assertEquals("MANGOES", max);
+    }
+
+    //Test Cases For Integer List
+    @Test
+    public void givenListOfIntegers_ShouldReturn_MaxValue() {
+        List<Integer> integerList = new LinkedList<>();
+        integerList.add(40);
+        integerList.add(60);
+        integerList.add(10);
+        integerList.add(25);
+        Object max = new Maximum<>(integerList).findMax();
+        Assert.assertEquals(60, max);
+    }
+
+    // Test Cases For Float List
+    @Test
+    public void givenListOfFloats_ShouldReturn_MaxValue() {
+        List<Float> floatList = new LinkedList<>();
+        floatList.add(50f);
+        floatList.add(60f);
+        floatList.add(20f);
+        floatList.add(80f);
+        Object max = new Maximum<>(floatList).findMax();
+        Assert.assertEquals(80f, max);
+    }
+
+    // Test Cases For String List
+    @Test
+    public void givenListOfStrings_ShouldReturn_MaxValue() {
+        List<String> stringList = new LinkedList<>();
+        stringList.add("MANGOES");
+        stringList.add("DAS");
+        stringList.add("HELLO");
+        stringList.add("BISWA");
+        Object max = new Maximum<>(stringList).findMax();
         Assert.assertEquals("MANGOES", max);
     }
 }
